514a112045e822c1326084d4996b6cc0c1335841
Konstantin Elagin <k.elagin@sape.ru>
2017-10-30 18:15:12 +0300

    ch

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#funnel').find('[data-logos-section]').each(function(){
+        var section_id = $(this).attr('data-logos-section');
+        var parsed_block =  $('#logos').find('[data-logos-section =' + section_id + ']').html();
+        $(this).html(parsed_block);
+    });
+
+
+    var sliderLogos = new Swiper('.funnel__slider', {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      centeredSlides: true,
+      wrapperClass: 'funnel__slider-wrapper',
+      slideClass: 'logos__item'
+    });
+
+
+});
